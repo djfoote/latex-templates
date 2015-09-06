49d395366c14879da91f04b6fd1eee2f0c47445f
Davis Foote <djfoote@berkeley.edu>
2015-09-06 12:07:15 -0700

    Install script works without kpsepath
    Defaults to ~/Library/texmf

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
-for path in $(kpsepath tex | tr : '\n'); do
+copyall() {
+    for file in *.cls; do
+        cp $file $1
+    done
+    for file in *.sty; do
+        cp $file $1
+    done
+}
+
+if hash kpsepath 2>/dev/null; then
+    paths=$(kpsepath tex | tr : '\n')
+else
+    paths="$HOME/Library/texmf"
+fi
+
+for path in $paths; do
     if [ "$path" != "." ] && [ -d $path ] && [ ${path:0:1} != "!" ]; then
-        for file in *.cls; do
-            cp $file $path
-        done
-        for file in *.sty; do
-            cp $file $path
-        done
+        copyall $path
         echo "Installation successful at location $path"
         exit
     fi
@@ -13,18 +23,13 @@ done
 
 echo "No previously existing possible installation paths."
 
-for path in $(kpsepath tex | tr : '\n'); do
+for path in $paths; do
     if [ "$path" != "." ] && [ ${path:0:1} != "!" ]; then
         read -p "Create installation path $path [Y/n]?" -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]] ; then
             mkdir -p $path
-            for file in *.cls; do
-                cp $file $path
-            done
-            for file in *.sty; do
-                cp $file $path
-            done
+            copyall $path
             echo "Installation successful at location $path"
             exit
         fi
